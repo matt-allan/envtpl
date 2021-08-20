5410a0c14704fe68c320ad3fce78a143759ad395
Matthew Allan <matt@mattallan.me>
2021-08-19 20:33:28 -0400

    Use simple parser approach

--- build.zig
+++ build.zig
@@ -11,7 +11,7 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("tplenv", "src/main.zig");
+    const exe = b.addExecutable("envtpl", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.install();
--- src/main.zig
+++ src/main.zig
@@ -1,37 +1,36 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const allocator = std.heap.page_allocator;
     const stdin = std.io.bufferedReader(std.io.getStdIn().reader()).reader();
     var stdout = std.io.bufferedWriter(std.io.getStdOut().writer());
 
     var buf: [1024]u8 = undefined;
 
-    var env_map = try std.process.getEnvMap(allocator);
-    defer env_map.deinit();
+    var closingBracePos: ?usize = null;
 
     while (try stdin.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        var iter = env_map.iterator();
+        for (line) |c, i| {
+            switch (c) {
+                '$' => {
+                    closingBracePos = std.mem.indexOfPos(u8, line, i + 1, "}");
 
-        var replaced = line;
+                    if (closingBracePos) |end| {
+                        const name = line[i + 2 .. end];
+                        const value = std.os.getenv(name);
 
-        while (iter.next()) |entry| {
-            const key = entry.key_ptr.*;
-            const value = entry.value_ptr.*;
-
-            var tplKey = try std.fmt.allocPrint(allocator, "${{{s}}}", .{key});
-
-            const outputSize = std.mem.replacementSize(u8, replaced, tplKey, value);
-
-            var output = try allocator.alloc(u8, outputSize);
-
-            var replacements = std.mem.replace(u8, replaced, tplKey, value, output[0..]);
-
-            replaced = output;
+                        try stdout.writer().print("{s}", .{value});
+                    } else {
+                        try stdout.writer().print("{c}", .{c});
+                    }
+                },
+                else => {
+                    if (closingBracePos == null or closingBracePos orelse 0 < i) {
+                        try stdout.writer().print("{c}", .{c});
+                    }
+                },
+            }
         }
 
-        try stdout.writer().print("{s}\n", .{replaced});
+        try stdout.flush();
     }
-
-    try stdout.flush();
 }
